8628a99a318cd451816505a72a5963009e367ef2
Alina <K.alinka555@list.ru>
2021-09-03 19:42:02 +0300

    Подготовлен скелет проекта и первые файлы

---
+++ src/main/java/fullstack/water_mill/WaterMillApplication.java
@@ -0,0 +1,13 @@
+package fullstack.water_mill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterMillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterMillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fullstack/water_mill/WaterMillApplicationTests.java
@@ -0,0 +1,13 @@
+package fullstack.water_mill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterMillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
